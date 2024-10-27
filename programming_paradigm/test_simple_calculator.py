4e41688c9fd0cab0b4951e61c706517c47367934
Elikemuriah <164448270+Elikemuriah@users.noreply.github.com>
2024-10-27 23:42:04 +0000

    Update test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -9,29 +9,29 @@ class TestSimpleCalculator(unittest.TestCase):
 
     def test_add(self):
         #Test addition method.
-        self.assertEqual(self.calculator.add(5, 3), 8)
-        self.assertEqual(self.calculator.add(-1, 1), 0)
-        self.assertEqual(self.calculator.add(0, 0), 0)
+        self.assertEqual(self.calc.add(5, 3), 8)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(0, 0), 0)
 
     def test_subtract(self):
         #Test subtraction method.
-        self.assertEqual(self.calculator.subtract(5, 3), 2)
-        self.assertEqual(self.calculator.subtract(-1, 1), -2)
-        self.assertEqual(self.calculator.subtract(0, 0), 0)
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(-1, 1), -2)
+        self.assertEqual(self.calc.subtract(0, 0), 0)
 
     def test_multiply(self):
         #Test multiplication method.
-        self.assertEqual(self.calculator.multiply(5, 3), 15)
-        self.assertEqual(self.calculator.multiply(-1, 1), -1)
-        self.assertEqual(self.calculator.multiply(0, 10), 0)
+        self.assertEqual(self.calc.multiply(5, 3), 15)
+        self.assertEqual(self.calc.multiply(-1, 1), -1)
+        self.assertEqual(self.calc.multiply(0, 10), 0)
 
     def test_divide(self):
         #Test division method, including division by zero.
-        self.assertEqual(self.calculator.divide(6, 3), 2)
-        self.assertEqual(self.calculator.divide(-6, 3), -2)
-        self.assertEqual(self.calculator.divide(0, 3), 0)
+        self.assertEqual(self.calc.divide(6, 3), 2)
+        self.assertEqual(self.calc.divide(-6, 3), -2)
+        self.assertEqual(self.calc.divide(0, 3), 0)
         # Test division by zero
-        self.assertIsNone(self.calculator.divide(5, 0), "Expected None when dividing by zero")
+        self.assertIsNone(self.calc.divide(5, 0), "Expected None when dividing by zero")
 
 if __name__ == "__main__":
     unittest.main()
