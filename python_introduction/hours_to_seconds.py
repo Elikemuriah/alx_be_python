87e034e7e8f5ca8ac7c3368336f2d5c5817ddff2
Elikemuriah <164448270+Elikemuriah@users.noreply.github.com>
2024-10-27 22:13:02 +0000

    Update hours_to_seconds.py

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -3,7 +3,7 @@ minutes = 60
 min_seconds = 60
 
 #CONVERTING SECONDS TO ONE HOUR
-one_hour_seconds = min_seconds * minutes
+#one_hour_seconds = min_seconds * minutes
 
 #CONVERTING TO 2 HOURS
 #hour_seconds = one_hour_seconds * hours
